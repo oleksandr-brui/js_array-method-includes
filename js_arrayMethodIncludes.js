c507afea465555cdce532614b1eb44071dfe62b5
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-22 22:35:35 +0200

    Implementation of the method .includes() in native Java Script

--- js_arrayMethodIncludes.js
+++ js_arrayMethodIncludes.js
@@ -1,1 +1,40 @@
 'use strict';
+
+numbers.includes = function(valueToFind, fromIndex = 0) {
+  // write code here
+  const { length: arrLength } = this;
+
+  if (isNaN(fromIndex)) {
+    for (let i = 0; i < arrLength; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+  }
+
+  if (fromIndex >= 0) {
+    for (let i = fromIndex; i < arrLength; i++) {
+      if (this[i] === valueToFind) {
+        return true;
+      }
+    }
+  }
+
+  for (let i = 0; i < arrLength; i++) {
+    if (this[i] === valueToFind) {
+      return true;
+    }
+  }
+
+  if (fromIndex < 0) {
+    const startIndex = arrLength + fromIndex;
+
+    for (let i = startIndex; i < arrLength; i++) {
+      if (this[i] === valueToFind) {
+        return i;
+      }
+    }
+  }
+
+  return false;
+};
